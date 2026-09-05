e16019300d156ac3497271ea331d1b962add4e57
Subarna_paudel623 <subarnapaudel623@gmail.com>
2026-09-05 11:52:45 +0545

    Add files via upload
    dictionary

---
+++ 4.1_dictionary.py
@@ -0,0 +1,9 @@
+marks = {
+  "Subarna": 100,
+  "Shubham": 200,
+  "Rohan": 23
+}
+# print(marks, type(marks))
+# print(marks[0]) error
+print(marks["Subarna"])
+
---
+++ 4.2_dictionary_methods.py
@@ -0,0 +1,43 @@
+
+student = {
+  "name": "subarna",
+  "age" : 23,
+  "address": "kohalpur",
+  "course" : "pyhton"
+}
+
+
+# #  1 get()
+# print(student.get("name"))
+# print(student.get("address"))
+
+# #  2. keys()
+# print(student.keys())
+
+# # 3. Values()
+# print(student.values())
+
+
+# # 4. items()
+# print(student.items())
+
+
+# # # update()
+# student.update(
+#   {
+#     "age":25,
+#     "address": "Nepal"
+#   }
+# )
+# print(student)
+
+
+# # pop()
+# student.pop("age")
+# print(student)
+
+# clear()
+student.clear()
+print(student)
+
+
