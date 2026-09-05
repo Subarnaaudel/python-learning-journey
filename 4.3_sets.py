db8cca5ecdc9d94cfd4394c6bdf96dfccfcdf549
Subarna_paudel623 <subarnapaudel623@gmail.com>
2026-09-05 18:13:47 +0545

    Add files via upload

---
+++ 4.3_sets.py
@@ -0,0 +1,5 @@
+e = set() #dont use s ={} as it will create an empty dictionary
+s ={1, 2, 3,4, 54,5,6}
+print(s)
+
+
---
+++ 4.4_set_methods.py
@@ -0,0 +1,26 @@
+numbers={1,2,3,4,5,6,7}
+
+
+
+# # 1 add()
+# numbers.add(4)
+# print(numbers)
+
+
+# # 2 Update()
+# numbers.update([9,10,11])
+# print(numbers)
+
+# # 3 remove()
+# numbers.remove(5)
+# print(numbers)
+
+
+# # 4 discard()
+# numbers.discard(1)
+# print(numbers)
+
+# #  5 union
+# number = {12,15,56}
+# print(numbers.union(number))
+
